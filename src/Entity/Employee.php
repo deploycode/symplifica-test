534319f5d893b8dcd2cc65d0ef1a5fdaffda49f5
Gabriel Rodríguez <gabriel@deploycode.co>
2020-07-12 20:45:54 -0500

    php bin/console make:entity

---
+++ src/Entity/Employee.php
@@ -0,0 +1,203 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\EmployeeRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=EmployeeRepository::class)
+ */
+class Employee
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=55)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=15)
+     */
+    private $gender;
+
+    /**
+     * @ORM\Column(type="string", length=15)
+     */
+    private $identification_number;
+
+    /**
+     * @ORM\Column(type="string", length=15)
+     */
+    private $phone_number;
+
+    /**
+     * @ORM\Column(type="string", length=55, nullable=true)
+     */
+    private $address;
+
+    /**
+     * @ORM\Column(type="date", nullable=true)
+     */
+    private $date_of_birth;
+
+    /**
+     * @ORM\Column(type="string", length=15, nullable=true)
+     */
+    private $type_of_contract;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Employee::class, inversedBy="employees")
+     */
+    private $boss;
+
+    /**
+     * @ORM\OneToMany(targetEntity=Employee::class, mappedBy="boss")
+     */
+    private $employees;
+
+    public function __construct()
+    {
+        $this->employees = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getGender(): ?string
+    {
+        return $this->gender;
+    }
+
+    public function setGender(string $gender): self
+    {
+        $this->gender = $gender;
+
+        return $this;
+    }
+
+    public function getIdentificationNumber(): ?string
+    {
+        return $this->identification_number;
+    }
+
+    public function setIdentificationNumber(string $identification_number): self
+    {
+        $this->identification_number = $identification_number;
+
+        return $this;
+    }
+
+    public function getPhoneNumber(): ?string
+    {
+        return $this->phone_number;
+    }
+
+    public function setPhoneNumber(string $phone_number): self
+    {
+        $this->phone_number = $phone_number;
+
+        return $this;
+    }
+
+    public function getAddress(): ?string
+    {
+        return $this->address;
+    }
+
+    public function setAddress(?string $address): self
+    {
+        $this->address = $address;
+
+        return $this;
+    }
+
+    public function getDateOfBirth(): ?\DateTimeInterface
+    {
+        return $this->date_of_birth;
+    }
+
+    public function setDateOfBirth(?\DateTimeInterface $date_of_birth): self
+    {
+        $this->date_of_birth = $date_of_birth;
+
+        return $this;
+    }
+
+    public function getTypeOfContract(): ?string
+    {
+        return $this->type_of_contract;
+    }
+
+    public function setTypeOfContract(?string $type_of_contract): self
+    {
+        $this->type_of_contract = $type_of_contract;
+
+        return $this;
+    }
+
+    public function getBoss(): ?self
+    {
+        return $this->boss;
+    }
+
+    public function setBoss(?self $boss): self
+    {
+        $this->boss = $boss;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|self[]
+     */
+    public function getEmployees(): Collection
+    {
+        return $this->employees;
+    }
+
+    public function addEmployee(self $employee): self
+    {
+        if (!$this->employees->contains($employee)) {
+            $this->employees[] = $employee;
+            $employee->setBoss($this);
+        }
+
+        return $this;
+    }
+
+    public function removeEmployee(self $employee): self
+    {
+        if ($this->employees->contains($employee)) {
+            $this->employees->removeElement($employee);
+            // set the owning side to null (unless already changed)
+            if ($employee->getBoss() === $this) {
+                $employee->setBoss(null);
+            }
+        }
+
+        return $this;
+    }
+}
