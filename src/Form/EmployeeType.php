b25ac5e7578db0b29e424215c420526f51e13b2d
Gabriel Rodríguez <gabriel@deploycode.co>
2020-07-12 21:19:01 -0500

    php bin/console make:migration php bin/console make:crud Product

---
+++ src/Form/EmployeeType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Employee;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EmployeeType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('gender')
+            ->add('identification_number')
+            ->add('phone_number')
+            ->add('address')
+            ->add('date_of_birth')
+            ->add('type_of_contract')
+            ->add('boss')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Employee::class,
+        ]);
+    }
+}
