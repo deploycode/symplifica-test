37515869ac52a6eca8da028ed921ac0efc9dbf82
Gabriel Rodríguez <gabriel@deploycode.co>
2020-07-12 20:52:41 -0500

    php bin/console make:migration

---
+++ src/Migrations/Version20200713014844.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200713014844 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE employee (id INT AUTO_INCREMENT NOT NULL, boss_id INT DEFAULT NULL, name VARCHAR(55) NOT NULL, gender VARCHAR(15) NOT NULL, identification_number VARCHAR(15) NOT NULL, phone_number VARCHAR(15) NOT NULL, address VARCHAR(55) DEFAULT NULL, date_of_birth DATE DEFAULT NULL, type_of_contract VARCHAR(15) DEFAULT NULL, INDEX IDX_5D9F75A1261FB672 (boss_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE employee ADD CONSTRAINT FK_5D9F75A1261FB672 FOREIGN KEY (boss_id) REFERENCES employee (id)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE employee DROP FOREIGN KEY FK_5D9F75A1261FB672');
+        $this->addSql('DROP TABLE employee');
+    }
+}
